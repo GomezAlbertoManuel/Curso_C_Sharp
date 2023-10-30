274e966452b105ed9163255c611a37c39c141e37
Manolo <lavalle903@gmail.com>
2023-10-30 20:35:08 -0300

    Properties

---
+++ POO/_011_Properties/Empleado.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011_Properties
+{
+    internal class Empleado
+    {
+        private string nombre;
+        private string apellido;
+        private double salario;
+        private string contraseña;
+        public Empleado(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        // El metodo "IngresarSalario" es "private" porque solo va a ser usado por la propiedad set de "Salario"
+        // no es necesario hacerlo publico.
+        private double IngresarSalario(double nuevoSalario)
+        {
+            while (nuevoSalario < 0)
+            {
+                Console.WriteLine("El salario no puede ser negativo.\r\n \r\n Ingrese nuevo salario:");
+                nuevoSalario = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("El nuevo salario asignado es: $" + nuevoSalario);
+            return this.salario = nuevoSalario;
+        }
+
+        // La convención general para nombrar propiedades es utilizar el formato "PascalCase"
+        // El "get" no tiene mucha ciencia, solo devuelve el valor que le asignemos
+        // Pero el "set" nos da la ventaja que podemos asignar un metodo para validar el valor ingresado.
+        public double Salario 
+        {            
+            get { return salario; }            
+            set { salario = IngresarSalario(value); }
+        }
+
+
+        // Las propiedades pueden ser de solo lectura.
+        // en este caso accedemos al nombre ingresado en el constructor.
+        public string Nombre 
+        {
+            get { return nombre; }
+        }
+
+        // Tambien pueden ser de solo escritura, como en el caso de las contraseñas. 
+        public string Contraseña 
+        {
+            set { contraseña = ValidarContraseña(value); }
+        }
+
+        private string ValidarContraseña (string contraseña)
+        {
+            String texto = "Contraseña Asignada";
+
+            return texto;
+        }
+
+
+        // -------------------------------------------------------------------------------------------
+        // Declaracion simplificada (expression bodied)
+        public double Salario2 
+        {
+            get => salario;
+            set => salario = IngresarSalario(value);
+        }
+
+
+
+        // -------------------------------------------------------------------------------------------
+        // Propiedad atomatica
+        // Puedes acceder y modificar el valor de esta propiedad como si fuera un campo público,
+        // pero en realidad, está respaldado por un campo privado y el compilador se encarga de generar los métodos get y set necesarios.
+        public string Apelido { get; set; }
+
+    }
+}
---
+++ POO/_011_Properties/PropertiesProgram.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011_Properties
+{
+    internal class PropertiesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("" +
+                "En C#, las propiedades son miembros de una clase que permiten el acceso y la modificación de los " +
+                "atributos o campos privados de la clase de una manera controlada.\r\n\r\n" +
+                "Las propiedades proporcionan una forma de encapsular el acceso a los campos privados de una clase, " +
+                "lo que significa que puedes definir cómo se obtiene y establece el valor de un atributo sin exponer" +
+                "directamente el campo subyacente.\r\n\r\n" +
+                "Una propiedad consta de dos bloques: el 'get' (obtención) y el 'set' (establecimiento). El bloque 'get'" +
+                "define cómo se recupera el valor de la propiedad, y el bloque 'set' define cómo se asigna un valor a la" +
+                "propiedad.\r\n\r\n" +
+                "Por ejemplo, si tienes una clase con un campo 'edad', puedes definir una propiedad 'Edad' que permita" +
+                "obtener y establecer la edad de un objeto de la siguiente manera:\r\n" +
+                "public int Edad\r\n" +
+                "{\r\n" +
+                "    get { return edad; } // Obtener el valor del campo 'edad'\r\n" +
+                "    set { edad = value; } // Establecer el valor del campo 'edad'\r\n" +
+                "}\r\n\r\n" +
+                "Cuando trabajas con propiedades, puedes agregar lógica personalizada en los bloques 'get' y 'set' para" +
+                "realizar validaciones, cálculos u otras operaciones necesarias. Además, las propiedades permiten controlar" +
+                "quiénes y cómo acceden a los campos de la clase, ya que puedes establecer diferentes niveles de acceso," +
+                "como público, privado o protegido.\r\n\r\n" +
+                "En resumen, las propiedades en C# son una forma eficaz de proporcionar acceso a los atributos de una clase" +
+                "mientras se mantiene el control sobre cómo se obtiene y establece el valor, lo que favorece la encapsulación" +
+                "y la seguridad de los datos.");
+
+
+
+            Console.WriteLine("__________________________________________________\r\n \r\n");
+
+            Empleado empleado = new Empleado("Manuel");
+
+            Console.WriteLine($"Hola ingresa el nuevo salario para: {empleado.Nombre}");
+
+            empleado.Salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingresa una contraseña, para asignar a " + empleado.Nombre);
+
+            empleado.Contraseña = Console.ReadLine();
+                                 
+
+            Console.WriteLine("No se definio apellido para: " + empleado.Nombre  + "\r\n" +
+                "Por favor, escriba el apellido");
+
+            empleado.Apelido = Console.ReadLine();
+            Console.WriteLine($"\r\nSe ha creado el empleado {empleado.Nombre} {empleado.Apelido}. \r\nSu Salario Inicial es: ${empleado.Salario}");
+
+            
+        }
+    }
+}
